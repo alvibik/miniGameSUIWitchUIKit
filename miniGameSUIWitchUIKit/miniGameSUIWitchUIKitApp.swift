c54e88ed2dc065023779877ce5a856d7969791de
Александр Биктеев <bikteev@mail.ru>
2022-11-30 15:36:29 +0600

    Initial Commit

---
+++ miniGameSUIWitchUIKit/miniGameSUIWitchUIKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  miniGameSUIWitchUIKitApp.swift
+//  miniGameSUIWitchUIKit
+//
+//  Created by albik on 30.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct miniGameSUIWitchUIKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
